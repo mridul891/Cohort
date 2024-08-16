68a246a21484beb2fe75072fbb7828901cd50fb4
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-16 22:45:26 +0530

    custom hook for data fetching

--- customhooks/hooks/src/App.jsx
+++ customhooks/hooks/src/App.jsx
@@ -1,26 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0);
+function useTodo(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <></>;
+  useEffect(() => {
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
+
+    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+
+    // we are writing this clean up function as if we n changes then we have 2 call of hthe set internval i.e first for the old n and then of the new n
+
+    return () => {
+      clearInterval(value);
+    };
+  }, [n]);
+  return { todos, loading };
 }
 
-function Mycomponent() {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
+function App() {
+  const { todos, loading } = useTodo(5);
+  if (loading) {
+    return <div>Loadding...</div>;
+  }
   return (
     <>
-      <div>
-        <p>{count}</p>
-        <button onClick={incrementCount}> Click</button>
-      </div>
+      {todos.map((todo, index) => (
+        <Track todo={todo} key={index} />
+      ))}
     </>
   );
 }
+
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
+}
+
 export default App;
